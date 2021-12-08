600ea033304cb3f1ce0ba56480b737de6fcc4638
svrnm <neumanns@cisco.com>
2021-12-08 16:49:57 +0100

    Re-add env.sh

---
+++ env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Your global account name
+export ACCOUNT_NAME="customer1_0974e892-a739-40f8-840c-c60d21c1a2e7"
+# An API key created via Analytics > Configuration > API Keys
+export API_KEY="..."
+# Your analytics endpoint
+export ENDPOINT="http://localhost:9080"
+# Your controller URL
+export APPD_CONTROLLER="http://localhost:8090"
+# Your controller credentials
+export APPD_CREDENTIALS="admin@customer1:appdynamics123"
+# The name of the schema used for your incident events
+export SCHEMA_NAME="incident_events"
+
